5706cd6b5aa5d8328acd2dad2a3f7f53edd963a9
kravchuk21 <krauchukvlad@gmail.com>
2023-11-11 22:54:47 +0300

    feat: Parsing Command Line Arguments

--- piff.py
+++ piff.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
 
-print("Hello, World!")
+import sys
+
+if __name__ == "__main__":
+    program = sys.argv[0]
+    if len(sys.argv) < 3:
+        print(f"Usage: {program} <file1> <file2>")
+        print(f"ERROR: no files to compare ")
+        exit(1)
+    file_path1 = sys.argv[1]
+    file_path2 = sys.argv[2]
+    print(f"File1: {file_path1}")
+    print(f"File2: {file_path2}")
