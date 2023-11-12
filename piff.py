9c7838b4b4bdc74c1a06cd1c4e91f87bef420ad4
kravchuk21 <krauchukvlad@gmail.com>
2023-11-12 14:05:10 +0300

    feat: Reading files

--- piff.py
+++ piff.py
@@ -2,13 +2,15 @@
 
 import sys
 
+def read_entire_file(file_path):
+    with open(file_path, "r") as f:
+        return f.read()
+
 if __name__ == "__main__":
     program = sys.argv[0]
     if len(sys.argv) < 3:
         print(f"Usage: {program} <file1> <file2>")
         print(f"ERROR: no files to compare ")
         exit(1)
-    file_path1 = sys.argv[1]
-    file_path2 = sys.argv[2]
-    print(f"File1: {file_path1}")
-    print(f"File2: {file_path2}")
+    lines1 = read_entire_file(sys.argv[1]).splitlines()
+    lines2 = read_entire_file(sys.argv[2]).splitlines()
